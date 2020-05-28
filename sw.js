b5aa20076fbd8367aad8e56e60ae591b6845827d
jsamek <jakubpsamek@gmail.com>
2020-05-28 14:24:34 +0200

    fixed

--- sw.js
+++ sw.js
@@ -1,15 +1,22 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('sw-cache').then(function(cache) {
-      return cache.add('pwa.html');
-    })
-  );
-});
+self.addEventListener('install', e => {
+  e.waitUntil(
+     caches.open('airhorner').then(cache => {
+         return cache.addAll([
+             '/'
+         ])
+             .then(() => self.skipWaiting());
+     })
+   )
+ });
  
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
+ self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+ });
+ 
+ self.addEventListener('fetch', event => {
+   event.respondWith(
+     caches.match(event.request).then(response => {
+         return response || fetch(event.request);
+     })
+   );
+ });
