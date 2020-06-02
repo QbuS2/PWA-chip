d4736d651b04f890cc6fab566a8241662794c1f6
jsamek <jakubpsamek@gmail.com>
2020-06-02 12:08:25 +0200

    fix touch

--- main.js
+++ main.js
@@ -10,15 +10,14 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 btnInstall = document.getElementById('install-button');
+
 btnInstall.addEventListener('click', () => {
   // Update the install UI to remove the install button
-  document.querySelector('#install-button').disabled = true;
-  // Show the modal add to home screen dialog
   installPromptEvent.prompt();
   // Wait for the user to respond to the prompt
   installPromptEvent.userChoice.then((choice) => {
     if (choice.outcome === 'accepted') {
-      //console.log('User accepted the A2HS prompt');
+      btnInstall.style.display = "none";
     } else {
       //console.log('User dismissed the A2HS prompt');
     }
