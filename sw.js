cec6a122045b3e90a91824f2a014532cbe06248f
jsamek <jakubpsamek@gmail.com>
2020-06-03 15:17:57 +0200

    changes

--- sw.js
+++ sw.js
@@ -1,22 +1,29 @@
+var isTooSoon = true;
+self.addEventListener("beforeinstallprompt", function (e) {
+  if (isTooSoon) {
+    e.preventDefault(); // Prevents prompt display
+    // Prompt later instead:
+    setTimeout(function () {
+      isTooSoon = false;
+      e.prompt(); // Throws if called more than once or default not prevented
+    }, 10000);
+  }
+  // Update the install UI to notify the user app can be installed
+  document.querySelector('#install-button').disabled = false;
+});
+
 self.addEventListener('install', e => {
   e.waitUntil(
-     caches.open('airhorner').then(cache => {
-         return cache.addAll([
-             '/'
-         ])
-             .then(() => self.skipWaiting());
-     })
-   )
- });
- 
- self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
- });
- 
- self.addEventListener('fetch', event => {
-   event.respondWith(
-     caches.match(event.request).then(response => {
-         return response || fetch(event.request);
-     })
-   );
- });
+    caches.open('airhorner').then(cache => {
+      return cache.addAll([
+        '/index.html',
+        '/Chip192.png',
+        '/Chip512.png',
+        '/style.css',
+        '/serviceWorker.js',
+        '/manifest.json',
+      ])
+        .then(() => self.skipWaiting());
+    })
+  )
+});
