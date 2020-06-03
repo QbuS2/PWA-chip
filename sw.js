e95fd2d353ef2a8522c62b7a75a27f041fda05a3
jsamek <jakubpsamek@gmail.com>
2020-06-03 16:12:50 +0200

    fix sw

--- sw.js
+++ sw.js
@@ -1,29 +1,44 @@
-var isTooSoon = true;
-self.addEventListener("beforeinstallprompt", function (e) {
-  if (isTooSoon) {
-    e.preventDefault(); // Prevents prompt display
-    // Prompt later instead:
-    setTimeout(function () {
-      isTooSoon = false;
-      e.prompt(); // Throws if called more than once or default not prevented
-    }, 10000);
-  }
-  // Update the install UI to notify the user app can be installed
-  document.querySelector('#install-button').disabled = false;
+self.addEventListener('install', event => {
+  event.waitUntil(
+      caches.open('v1').then(cache => {
+          return cache.addAll([
+            "/chip_ui/DEMO/e43127ad9018ad11dcc0a695356dbbb3/pwa/index.html",
+            "/chip_ui/DEMO/e43127ad9018ad11dcc0a695356dbbb3/pwa/style.css",
+            "/chip_ui/DEMO/e43127ad9018ad11dcc0a695356dbbb3/pwa/Chip192.png",
+            "/chip_ui/DEMO/e43127ad9018ad11dcc0a695356dbbb3/pwa/Chip512.png"
+          ]);
+      }, error => {
+          console.log(`Installation failed with error: ${error}`);
+      })
+  );
 });
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('airhorner').then(cache => {
-      return cache.addAll([
-        '/index.html',
-        '/Chip192.png',
-        '/Chip512.png',
-        '/style.css',
-        '/serviceWorker.js',
-        '/manifest.json',
-      ])
-        .then(() => self.skipWaiting());
-    })
-  )
+self.addEventListener('activate', event => {
+  let cacheKeepList = ['v1'];
+  
+  event.waitUntil(
+      caches.keys().then( keyList => {
+          return Promise.all(keyList.map(function(key) {
+              if (cacheKeepList.indexOf(key) === -1) {
+                  return caches.delete(key);
+              }
+          }));
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method != 'GET') return;
+  
+  event.respondWith(async function() {
+      const cache = await caches.open('v1');
+      const cachedResponse = await cache.match(event.request);
+      
+      if (cachedResponse) {
+          event.waitUntil(cache.add(event.request));
+          return cachedResponse;
+      }
+      
+      return fetch(event.request);
+  }());
 });
