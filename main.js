332dfc31b720c2b003e5f06975f632aaa200f2aa
Jaroslaw Matura <jaroslaw.a.matura@capgemini.com>
2020-06-05 17:36:33 +0200

    add flag list

---
+++ main.js
@@ -0,0 +1,28 @@
+var flagContainer = document.getElementById("flagContainer");
+
+
+function addFlags(...args) {
+
+}
+
+addFlag('en');
+addFlag('br');
+addFlag('en');
+addFlag('en');
+addFlag('en');
+addFlag('en');
+
+function addFlag(language) {
+    var box = document.createElement('div');
+    box.innerHTML = `
+<div class="flag flag-${language}"></div>
+<div class="flag-text"><p>${language}</p></div>
+`;
+    box.setAttribute("onclick", "openBot()");
+    flagContainer.appendChild(box);
+}
+
+function openBot() {
+    console.error("Clicked!");
+    //window.location.href = "https://capgemini-eu.dev.amelia.com/Amelia/ui/capgeminidemo/login?embed=iframe&domainCode=capgeminidemo&language=en&myURL=https://chip.capgemini.com/clients/DEMO_conf/preview_c&hint=Hello world!&delay=5000";
+}
