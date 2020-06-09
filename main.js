3e1faea1772b74b9745bf3d2837d0b14048311ea
Jaroslaw Matura <jaroslaw.a.matura@capgemini.com>
2020-06-09 13:15:34 +0200

    add custom header

--- main.js
+++ main.js
@@ -1,30 +1,80 @@
-var flagContainer = document.getElementById("flagContainer");
+var  flagsContainer = document.getElementById("flagsContainer");
 
-
-function addFlags(...args) {
-
+var domains = {
+    en: {
+        domainCode: "capgeminidemo",
+        uiBundle: "capgeminidemo"
+    },
+    de: {
+        domainCode: "capgeminidemo",
+        uiBundle: "capgeminidemo"
+    },
+    fr: {
+        domainCode: "capgeminidemo",
+        uiBundle: "capgeminidemo"
+    },
+    es: {
+        domainCode: "capgeminidemo",
+        uiBundle: "capgeminidemo"
+    },
+    br: {
+        domainCode: "capgeminidemo",
+        uiBundle: "capgeminidemo"
+    },
+    it: {
+        domainCode: "capgeminidemo",
+        uiBundle: "capgeminidemo"
+    }
 }
 
-addFlag('br');
-addFlag('en');
-addFlag('es');
-addFlag('de');
-addFlag('fr');
-addFlag('it');
+addFlags();
+// addFlag('br');
+// addFlag('en');
+// addFlag('es');
+// addFlag('de');
+// addFlag('fr');
+// addFlag('it');
 
-function addFlag(language) {
+
+
+
+function addFlags() {
+    for (var language in domains) {
+        console.log("language '" + language + "' has domain: " + domains[language].domainCode);
+        addFlag(language, domains[language].domainCode, domains[language].uiBundle)
+    }    
+}
+
+
+function addFlag(language, domain, bundle) {
     var box = document.createElement('div');
+    box.className = "flag-container";
     box.innerHTML = `
-<div class="flag flag-${language}"></div>
-<div class="flag-text">
-    <p>${language}</p>
-</div>
-`;
-    box.setAttribute("onclick", "openBot()");
-    flagContainer.appendChild(box);
+    <div class="flag flag-${language}"></div>
+    <div class="flag-text">
+        <p>${language}</p>
+    </div>
+    `;
+    box.setAttribute("onclick", `openBot('${domain}', '${bundle}')`);
+     flagsContainer.appendChild(box);
 }
 
-function openBot() {
-    console.error("Clicked!");
-    //window.location.href = "https://capgemini-eu.dev.amelia.com/Amelia/ui/capgeminidemo/login?embed=iframe&domainCode=capgeminidemo&language=en&myURL=https://chip.capgemini.com/clients/DEMO_conf/preview_c&hint=Hello world!&delay=5000";
+function openBot(domain, bundle) {
+    console.warn("Clicked!");
+    // window.location.href = `https://capgemini-eu.dev.amelia.com/Amelia/ui/${bundle}/login?embed=iframe&domainCode=${domain}&language=en&hint=Hello world!&delay=5000`;
+}
+
+var collapseElement = document.getElementById("headerCollapse"); 
+var prevScrollpos =  flagsContainer.scrollTop;
+ flagsContainer.onscroll = function() {
+    var scrollHeight = document.getElementsByClassName('header-title')[0].clientHeight;
+    var currentScrollPos =  flagsContainer.scrollTop;
+    if( currentScrollPos > scrollHeight || prevScrollpos > scrollHeight) {
+        if (prevScrollpos > currentScrollPos) {
+            collapseElement.classList.remove("collapse")
+        } else {
+            collapseElement.classList.add("collapse")
+        }
+    }
+    prevScrollpos = currentScrollPos;
 }
